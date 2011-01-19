d0a8a85b84722dab5f766e328d9d23e8448ba00e
Ryan <rgnitz@gmail.com>
2011-01-18 19:50:07 -0500

    added tests.

--- test/dance.js
+++ test/dance.js
@@ -1,6 +1,13 @@
 
 db.dance.drop();
 
+for (var idx=0; idx < 100; idx++) {
+    db.dance.findOne();
+    db.dance.insert({n: 't'});
+    db.dance.update({x: 1}, {n: 'u'});
+    db.dance.remove({n: 't'});
+};
+
 for (var idx=0; idx < 30000; idx++) db.dance.findOne();
 
 for (var idx=0; idx < 30000; idx++) db.dance.insert({n: 't'});
