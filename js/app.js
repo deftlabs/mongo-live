ca29547c29c4d1db71110b854d92798dd5601abc
Ryan <rgnitz@gmail.com>
2011-01-18 21:29:08 -0500

    more charts and chart types (added percent chart).

--- js/app.js
+++ js/app.js
@@ -19,6 +19,29 @@ var currentServerStatus = [];
 var previousServerStatus = [];
 
 /**
+ * Start the app.
+ */
+function runApp() {
+    startServerStatusPoll('localhost', 28017);
+
+    createChart('opcounters', 'query', 'opcountersQueryChart', 'rgba(57, 20, 175, 1)', 'rgba(57, 20, 175, 0)', true);
+    createChart('opcounters', 'insert', 'opcountersInsertChart', 'rgba(135, 110, 215, 1)', 'rgba(135, 110, 215, 0)', true);
+    createChart('opcounters', 'update', 'opcountersUpdateChart', 'rgba 64, 171, 1)', 'rgba(18, 64, 171, 0)', true);
+    createChart('opcounters', 'delete', 'opcountersDeleteChart', 'rgba(255, 231, 115, 1)', 'rgba(255, 231, 115, 0)', true);
+    createChart('opcounters', 'command', 'opcountersCommandChart', 'rgba(255, 128, 64, 1)', 'rgba(255, 128, 64, 0)', true);
+    createChart('opcounters', 'getmore', 'opcountersGetmoreChart', 'rgba(191, 96, 48, 1)', 'rgba(191, 96, 48, 0)', true);
+    createChart('connections', 'current', 'connectionsCurrentChart', 'rgba(255, 255, 0, 1)', 'rgba(255, 255, 0, 0)', false);
+    createChart('extra_info', 'page_faults', 'pageFaultsChart', 'rgba(185, 247, 62, 1)', 'rgba(185, 247, 62, 0)', true);
+    createChart('backgroundFlushing', 'flushes', 'flushesChart', 'rgba(166, 137, 0, 1)', 'rgba(166, 137, 0, 0)', true);
+    createPercentChart('lockedPercentChart', 'rgba(191, 191, 48, 1)', 'rgba(191, 191, 48, 0)', 'globalLock', 'totalTime', 'globalLock', 'lockTime');
+    createPercentChart('idxMissPercentChart', 'rgba(166, 75, 0, 1)', 'rgba(166, 75, 0, 0)', 'indexCounters', 'btree.accesses', 'indexCounters', 'btree.misses');
+    createChart('mem', 'mapped', 'memMappedChart', 'rgba(255, 116, 0, 1)', 'rgba(255, 116, 0, 0)', false);
+    createChart('network', 'bytesIn', 'netInChart', 'rgba(255, 128, 64, 1)', 'rgba(255, 128, 64, 0)', true);
+    createChart('network', 'bytesOut', 'netOutChart', 'rgba(191, 96, 48, 1)', 'rgba(191, 96, 48, 0)', true);
+
+};
+
+/**
  * Start the interval lookup for server status.
  */
 function startServerStatusPoll(mongoHost, mongoPort) {
@@ -50,34 +73,71 @@ function extractServerStatusValue(serverStatus, group, identity) {
 };
 
 /**
- * Add a server statu value to a series for a group/identity.
+ * Add a server status value to a series for a group/identity.
  */
-function addServerStatusValueToSeries(series, group, identity) {
+function addServerStatusValueToSeries(series, group, identity, isCounter) {
     var x = (new Date()).getTime();
 
     var currentValue = extractServerStatusValue(currentServerStatus, group, identity);
-    var previousValue = extractServerStatusValue(previousServerStatus, group, identity);
-
+    
     var y = 0;
-    if (currentValue && previousValue && (currentValue > previousValue)) {
-        y = currentValue - previousValue;
-    }
+    if (isCounter) {
+        var previousValue = extractServerStatusValue(previousServerStatus, group, identity);
+   
+        if (currentValue && previousValue && (currentValue > previousValue)) {
+            y = currentValue - previousValue;
+        }
+    } else { y = currentValue; }
+    series.append(x, y);
+};
+
+/**
+ * Add a server status value to a series for a group/identity.
+ */
+function addPercentToTimeSeries(series, group1, identity1, group2, identity2) {
+    var x = (new Date()).getTime();
+
+    var current1 = extractServerStatusValue(currentServerStatus, group1, identity1);
+    var current2 = extractServerStatusValue(currentServerStatus, group2, identity2);
+
+    var previous1 = extractServerStatusValue(previousServerStatus, group1, identity1);
+    var previous2 = extractServerStatusValue(previousServerStatus, group2, identity2);
+    
+    var y = 0;
+
+    var x1 = previous2 - current2;
+    var y1 = previous1 - current1;
+
+    if (y1 != 0) { y = (((x1 / y1) * 1000) / 10); }
 
     series.append(x, y);
 };
 
 /**
- * Create a chart.
+ * Create the lock percentage chart.
  */
-function createChart(chartName, group, identity, divId) {
+function createPercentChart(divId, lineColor, fillColor, group1, identity1, group2, identity2) {
     var series = new TimeSeries();
-
-    setInterval(function() { addServerStatusValueToSeries(series, group, identity); }, 1000);
+    setInterval(function() { addPercentToTimeSeries(series, group1, identity1, group2, identity2); }, 1000);
 
     var chart = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', fillStyle: '#402817',  lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
-    chart.addTimeSeries(series, { strokeStyle: 'rgba(102, 204, 102, 1)', fillStyle: 'rgba(102, 204, 102, 0.2)', lineWidth: 3 });
+    chart.addTimeSeries(series, { strokeStyle: lineColor, fillStyle: fillColor, lineWidth: 3 });
     chart.streamTo(document.getElementById(divId), 1000);
 };
+
+/**
+ * Create a chart with a single time series.
+ */
+function createChart(group, identity, divId, lineColor, fillColor, isCounter) {
+    var series = new TimeSeries();
+
+    setInterval(function() { addServerStatusValueToSeries(series, group, identity, isCounter); }, 1000);
+
+    var chart = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', fillStyle: '#402817',  lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
+    chart.addTimeSeries(series, { strokeStyle: lineColor, fillStyle: fillColor, lineWidth: 3 });
+    chart.streamTo(document.getElementById(divId), 1000);
+};
+
 
 function queryServerStatus(mongoHost, mongoPort, success, failure, cmdError, notFound, serverError) {
     queryDb(('http://' + mongoHost + ':' + mongoPort + '/serverStatus'), success, failure, cmdError, notFound, serverError);
